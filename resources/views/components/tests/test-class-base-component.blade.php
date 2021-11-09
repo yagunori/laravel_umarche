b49d4350dd36cc8c083d720e4862dd8be86b8708
yagunori <92207170+yagunori@users.noreply.github.com>
2021-11-09 22:08:25 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネントです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
+</div>

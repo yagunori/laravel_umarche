e7669aafbbc63df6ce2cb74ef97a4fb7b59f1cf7
yagunori <92207170+yagunori@users.noreply.github.com>
2021-11-13 07:50:32 +0900

    shopDeleteCreate

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -8,6 +8,9 @@ use App\Models\Owner; //エロクアント
 use Illuminate\Support\Facades\DB; //クエリビルダー
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
+use Throwable;
+use App\Models\Shop;
+use Illuminate\Support\Facades\Log;
 
 class OwnersController extends Controller
 {
@@ -70,11 +73,28 @@ class OwnersController extends Controller
             'password' => 'required|string|confirmed|min:8'
         ]);
 
-        Owner::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        try{
+            DB::transaction(function () use($request) {
+                $owner = Owner::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ]);
+
+                Shop::create([
+                    'owner_id' => $owner->id,
+                    'name' => '店名を入力してください',
+                    'information' => '',
+                    'filename' => '',
+                    'is_selling' => true
+                ]);
+            }, 2);
+        }catch(Throwable $e){
+            Log::error($e);
+            throw $e;
+        }
+
+        
 
         return redirect()
         ->route('admin.owners.index')

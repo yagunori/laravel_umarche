2123bc3f49f73be5d2355afd310d9ca54ef4cea6
yagunori <92207170+yagunori@users.noreply.github.com>
2021-11-13 21:55:57 +0900

    shop edit, update

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -13,10 +13,41 @@
                 <form method="POST" action="{{ route('owner.shops.update', ['shop' => $shop->id ] )}}" enctype="multipart/form-data">
                     @csrf
                     <div class="-m-2">
+                        <div class="p-2 w-1/2 mx-auto">
+                            <div class="relative">
+                              <label for="name" class="leading-7 text-sm text-gray-600">店名 ※必須</label>
+                              <input type="text" id="name" name="name" value="{{ $shop->name }}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                <label for="information" class="leading-7 text-sm text-gray-600">店舗情報 ※必須</label>
+                                <textarea id="information" name="information" rows="10" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $shop->information }}</textarea>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <div class="w-32">
+                                        <x-shop-thumbnail :filename="$shop->filename" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                       <div class="p-2 w-1/2 mx-auto">
                         <div class="relative">
                           <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
                           <input type="file" id="image" name="image" accept="image/png, image/jpeg, image/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                        </div>
+                      </div>
+                      <div class="p-2 w-1/2 mx-auto">
+                        <div class="relative flex justify-around">
+                            <div>
+                                <input type="radio" name="is_selling" class="mr-2" value="1" @if($shop->is_selling === 1){ checked } @endif>販売中
+                            </div>
+                            <div>
+                                <input type="radio" name="is_selling" class="mr-2" value="0" @if($shop->is_selling === 0){ checked } @endif>停止中
+                            </div>
+                        </div>
+                          {{-- <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
+                          <input type="file" id="image" name="image" accept="image/png, image/jpeg, image/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"> --}}
                         </div>
                       </div>
                       <div class="p-2 w-full flex justify-around mt-4">

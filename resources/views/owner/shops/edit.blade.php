02f22377a46dc99fdbdd52d2e225b316a1252ef6
yagunori <92207170+yagunori@users.noreply.github.com>
2021-11-15 21:44:26 +0900

    product create

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -13,16 +13,6 @@
                 <form method="POST" action="{{ route('owner.shops.update', ['shop' => $shop->id ] )}}" enctype="multipart/form-data">
                     @csrf
                     <div class="-m-2">
-                        <div class="p-2 w-1/2 mx-auto">
-                            <div class="relative">
-                              <label for="name" class="leading-7 text-sm text-gray-600">店名 ※必須</label>
-                              <input type="text" id="name" name="name" value="{{ $shop->name }}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                            </div>
-                            <div class="p-2 w-1/2 mx-auto">
-                                <div class="relative">
-                                <label for="information" class="leading-7 text-sm text-gray-600">店舗情報 ※必須</label>
-                                <textarea id="information" name="information" rows="10" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $shop->information }}</textarea>
-                            </div>
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
                                     <div class="w-32">

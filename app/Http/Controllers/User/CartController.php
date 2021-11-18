8efa36b9e580be6dba37eda92098a68f90f67beb
yagunori <92207170+yagunori@users.noreply.github.com>
2021-11-19 06:58:48 +0900

    オーナー向けメール送信

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -10,6 +10,7 @@ use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 use App\Services\CartService;
 use App\Jobs\SendThanksMail;
+use App\jobs\SendOrderedMail;
 
 class CartController extends Controller
 {
@@ -52,14 +53,7 @@ class CartController extends Controller
 
     public function checkout(){
 
-        ////
-        $items = Cart::where('user_id', Auth::id())->get();
-        $products = CartService::getItemsInCart($items);
-        $user = User::findOrFail(Auth::id());
-
-        SendThanksMail::dispatch($products, $user);
-        dd('ユーザーメール送信テスト');
-        ////
+        
 
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
@@ -109,6 +103,19 @@ class CartController extends Controller
     }
 
     public function success(){
+
+        ////
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        $user = User::findOrFail(Auth::id());
+
+        SendThanksMail::dispatch($products, $user);
+        foreach($products as $product)
+        {
+            SendOrderedMail::dispatch($product, $user);
+        }
+        // dd('ユーザーメール送信テスト');
+        ////
         Cart::where('user_id', Auth::id())->delete();
 
         return redirect()->route('user.items.index');
